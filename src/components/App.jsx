e4c9434b61df226badd7739b233144058fd0c50e
Svetlana210 <svetik.may24@gmail.com>
2023-01-16 11:49:15 +0100

    F

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
 import TransactionHistory from './TransactionHistory/TransactionHistory';
 
-import user from '../Data/data.json';
+import user from '../Data/user.json';
 import data from '../Data/data.json';
 import friends from '../Data/friends.json';
 import transactions from '../Data/transactions.json';
